fd1dd0697ae4053eefd2e2b4969d61401e17fdb0
igorinium <igorumarov13@gmail.com>
2023-10-31 19:47:30 +0300

    Add project files.

---
+++ Hometask7.1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты сферы:");
+            double x;
+            double y;
+            double z;
+            double radius;
+
+            while (true)
+            {
+                Console.Write("Координата x: ");
+                string inputStr = Console.ReadLine();
+                bool isParseSuccess = double.TryParse(inputStr, out x);
+                if (isParseSuccess || string.IsNullOrEmpty(inputStr))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число");
+                }
+            }
+            while (true)
+            {
+                Console.Write("Координата y: ");
+                string inputStr = Console.ReadLine();
+                bool isParseSuccess = double.TryParse(inputStr, out y);
+                if (isParseSuccess || string.IsNullOrEmpty(inputStr))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число");
+                }
+            }
+            while (true)
+            {
+                Console.Write("Координата z: ");
+                string inputStr = Console.ReadLine();
+                bool isParseSuccess = double.TryParse(inputStr, out z);
+                if (isParseSuccess || string.IsNullOrEmpty(inputStr))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число");
+                }
+            }
+            while (true)
+            {
+                Console.Write("Введите радиус: ");
+                string inputStr = Console.ReadLine();
+                bool isParseSuccess = double.TryParse(inputStr, out radius);
+                if (isParseSuccess || string.IsNullOrEmpty(inputStr))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число");
+                }
+            }
+
+            Sphere sphere = new Sphere(x, y, z, radius);
+
+            double volume = sphere.GetVolume();
+            Console.WriteLine($"Объём сферы = {volume}");
+
+            double square = sphere.GetSquare();
+            Console.WriteLine($"Площадь поверхности шара = {square}");
+
+            string coordinates = sphere.GetCenter();
+            Console.WriteLine($"Коодринаты центра сферы = {coordinates}");
+
+            sphere.SetCenter(34, 12, 99);
+            string coordinates2 = sphere.GetCenter();
+            Console.WriteLine($"Коодринаты центра сферы = {coordinates2}");
+
+            bool isPointInSphere = sphere.IsPointInside(0, 2, -5);
+            Console.WriteLine($"Некая точка внутри сферы = {isPointInSphere}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hometask7.1/Sphere.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask7._1
+{
+    internal class Sphere
+    {
+        double _x = 0;
+        double _y = 0;
+        double _z = 0;
+        double _radius = 1;
+        public Sphere(double x, double y, double z, double radius)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+            if (radius > 0)
+            {
+                _radius = radius;
+            }
+        }
+        public Sphere()
+        {
+
+        }
+        public Sphere(double radius)
+        {
+            _radius = radius;
+        }
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                if (_radius > 0)
+                {
+                    _radius = value;
+                }
+            }
+        }
+
+        public double GetVolume()
+        {
+            double volume = (4.0 / 3) * Math.PI * (_radius * _radius * _radius);
+            return volume;
+        }
+
+        public double GetSquare()
+        {
+            double squareSurface = 4 * Math.PI * (_radius * _radius);
+            return squareSurface;
+        }
+
+        public string GetCenter()
+        {
+            string coordinates = $"{_x} {_y} {_z}";
+            return coordinates;
+        }
+
+        public void SetCenter(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public bool IsPointInside(double x, double y, double z)
+        {
+            bool isTrue = true;
+            if (!((_x + _radius) >= x && (_x - _radius) <= x))
+            {
+                isTrue = false;
+            }
+            if (!((_y + _radius) >= y && (_y - _radius) <= y))
+            {
+                isTrue = false;
+            }
+            if (!((_z + _radius) >= z && (_z - _radius) <= z))
+            {
+                isTrue = false;
+            }
+            return isTrue;
+        }
+    }
+}
